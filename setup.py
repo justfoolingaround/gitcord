0531970b0635f5d3365c9681a62cb8d645fde290
KR <79979949+justfoolingaround@users.noreply.github.com>
2022-06-04 23:06:52 +0545

    Chore: Remove mandatory `__version__.py` import

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+import re
+
 from setuptools import find_packages, setup
 
-from gitcord.__version__ import __version__
+
+with open("./gitcord/__version__.py") as version_py_file:
+     version_raw = version_py_file.read()
+
 
 with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
 
 setup(
     name="gitcord",
-    version=__version__,
+    version=re.search('^__version__ = "(.+?)"$', version_raw).group(1),
     author="kr@justfoolingaround",
     author_email="kr.justfoolingaround@gmail.com",
     description="Provide smart GitHub codeblocks from your Discord bot.",
